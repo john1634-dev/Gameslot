bc36dee6f785aecae6a3daf1ef70af220f588d89
john1634-dev <bleachacc0011@gmail.com>
2026-06-16 18:25:44 +0800

    Fix database config

--- Gameslot/settings.py
+++ Gameslot/settings.py
@@ -83,7 +83,9 @@ WSGI_APPLICATION = 'Gameslot.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
