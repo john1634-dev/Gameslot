9ba413ac9103b03ba2b2719fa2f96e9c519cc004
john1634-dev <bleachacc0011@gmail.com>
2026-06-16 14:54:27 +0800

    Fix Cloudinary static storage

--- Gameslot/settings.py
+++ Gameslot/settings.py
@@ -144,3 +144,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
