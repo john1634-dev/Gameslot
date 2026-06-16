5e5cbb1168c0a026ea757b314b7f86935ba31bbb
john1634-dev <bleachacc0011@gmail.com>
2026-06-16 18:06:03 +0800

    Use postgres database on vercel

--- Gameslot/settings.py
+++ Gameslot/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'Gameslot.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
